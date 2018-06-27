f04ec857892967792f0a4b8c913e52b62a05c85d
Sam Washington <sam@spwashi.com>
2018-06-27 13:11:43 -0500

    updated logging layer

--- src/Sm/Logging/LoggingLayer.php
+++ src/Sm/Logging/LoggingLayer.php
@@ -14,12 +14,38 @@ class LoggingLayer extends StandardLayer implements \Sm\Logging\Logger {
 	protected $loggers = [];
 	/** @var string $log_path */
 	protected $log_path;
+	/**
+	 * @param $item
+	 * @return bool
+	 */
+	public static function convertThrowableToLoggable($item): array {
+		$is_sm              = $item instanceof \Sm\Core\Exception\Exception;
+		$exception_messages = [
+			'success'   => false,
+			'type'      => get_class($item),
+			'message'   => $item->getMessage(),
+			'file'      => $item->getFile(),
+			'line'      => $item->getLine(),
+			'trace'     => \Sm\Core\Exception\Exception::getAbbreviatedTrace($item->getTrace()),
+			'monitors'  => $is_sm ? $item->getMonitorContainer() : null,
+			'previous'  => $item->getPrevious(),
+			'exception' => $item,
+		];
+		return $exception_messages;
+	}
 	public function __get($name) {
 		if (isset($this->loggers[$name])) return $this->loggers[$name];
 		return parent::__get($name);
 	}
-	public function log($item, string $name = 'info', $level = null) {
-		$level  = !empty($level) ? $level : Logger::INFO;
+	public function log($item, string $name = null, $level = null) {
+		$level = !empty($level) ? $level : Logger::INFO;
+
+		if ($item instanceof \Throwable) {
+			$item = self::convertThrowableToLoggable($item);
+			$name = $name ?? ($item instanceof \Sm\Core\Exception\Exception ? 'sm_exception' : 'exception');
+		}
+
+		$name   = $name ?? 'info';
 		$logger = $this->loggers[$name] ?? $this->createLogger($name);
 		return $logger->log($level, is_string($item) ? $item : json_encode($item));
 	}
